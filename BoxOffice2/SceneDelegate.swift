5005825faac9565c325d49a0c6ed37cf849a454f
wongbingg <wb277@naver.com>
2023-02-23 12:08:49 +0900

    feat : Initial Commit
    - 프로젝트 생성
    - 스토리보드 제거

---
+++ BoxOffice2/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
---
+++ BoxOffice2/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
