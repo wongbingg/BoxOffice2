9537d75c0cc473a0f17c440a80b0e0bb2504a536
wongbingg <wb277@naver.com>
2023-02-23 21:07:56 +0900

    refactor : MoviePosterResponseDTO+Mapping 모델 수정 #3
    - OMDb API 에서 네이버 API 로 변경

--- BoxOffice2/Data/Network/DataMapping/MoviePosterResponseDTO+Mapping.swift
+++ BoxOffice2/Data/Network/DataMapping/MoviePosterResponseDTO+Mapping.swift
@@ -8,39 +8,21 @@
 import Foundation
 
 struct MoviePosterResponseDTO: Decodable {
-    let search: [Movie]
-    let totalResults: String
-    let response: String
+    let items: [Item]
     
-    enum CodingKeys: String, CodingKey {
-        case search = "Search"
-        case totalResults
-        case response = "Response"
-    }
-    
-    func posterURLString() -> String? {
-        let posterURL = search[0].poster
-        if posterURL.count < 10 {
-            return nil
-        } else {
-            return posterURL
-        }
+    func posterURLString() -> String {
+        let posterURL = items[0].image
+        return posterURL
     }
 }
 
-struct Movie: Decodable {
+struct Item: Decodable {
     let title: String
-    let year: String
-    let imdbID: String
-    let type: String
-    let poster: String
-    
-    enum CodingKeys: String, CodingKey {
-        case title = "Title"
-        case year = "Year"
-        case imdbID
-        case type = "Type"
-        case poster = "Poster"
-    }
+    let link: String
+    let image: String
+    let subtitle: String
+    let pubDate: String
+    let director: String
+    let actor: String
+    let userRating: String
 }
-
