4b5d1c02fec384fdc22690047d74d955ee9ed6a1
wongbingg <wb277@naver.com>
2023-02-23 21:12:17 +0900

    refactor : MovieRepository 인터페이스 수정 #3

--- BoxOffice2/Domain/Interfaces/MovieRepository.swift
+++ BoxOffice2/Domain/Interfaces/MovieRepository.swift
@@ -5,7 +5,11 @@
 //  Created by 이원빈 on 2023/02/23.
 //
 
+import RxSwift
+
 protocol MovieRepository {
-    func fetchMovies() -> [MovieCellData]
-    func fetchMovieDetail() -> MovieDetailData
+    func fetchDailyMovies(date: String) -> Observable<[MovieCellData]>
+    func fetchWeekDaysMovies(date: String) -> Observable<[MovieCellData]>
+    func fetchWeekEndMovies(date: String) -> Observable<[MovieCellData]>
+    func fetchMovieDetail(movieData: MovieCellData) -> Observable<MovieDetailData>
 }
