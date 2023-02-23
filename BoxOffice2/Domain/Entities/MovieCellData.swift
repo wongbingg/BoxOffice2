68910b3df25c61e9497ca0939d66c1d1fe3b1115
wongbingg <wb277@naver.com>
2023-02-23 13:08:41 +0900

    feat : MovieCellData 생성 #1
    - 셀에 들어갈 정보를 담은 도메인 모델

---
+++ BoxOffice2/Domain/Entities/MovieCellData.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCellData.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/23.
+//
+
+struct MovieCellData {
+    let uuid: String
+    let posterURL: String
+    let currentRank: String
+    let title: String
+    let openDate: String
+    let totalAudience: String
+    let rankChange: String
+    let isNewEntry: Bool
+}
