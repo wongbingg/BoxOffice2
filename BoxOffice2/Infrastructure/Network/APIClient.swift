2d8f04231cc28938c8ecb2f590acd523f49ff07a
wongbingg <wb277@naver.com>
2023-02-23 21:15:06 +0900

    feat : APIClient 생성 #3
    - URLRequest 를 기반으로 데이터를 요청하는 객체 생성

---
+++ BoxOffice2/Infrastructure/Network/APIClient.swift
@@ -0,0 +1,43 @@
+//
+//  APIClient.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/23.
+//
+
+import Foundation
+import RxSwift
+
+struct APIClient {
+    static let shared = APIClient(sesseion: URLSession.shared)
+    private let session: URLSession
+    
+    func requestData(with urlRequest: URLRequest) -> Observable<Data> {
+        
+        return Observable.create { emitter in
+            let task = session.dataTask(with: urlRequest) { data, response, error in
+                guard error == nil else {
+                    emitter.onError(APIError.unknown)
+                    return
+                }
+                let successRange = 200..<300
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                      successRange.contains(statusCode) == false {
+                    emitter.onError(APIError.response(statusCode))
+                } else if let data = data {
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                }
+            }
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+
+    init(sesseion: URLSession) {
+        self.session = sesseion
+    }
+}
