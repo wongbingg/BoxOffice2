28beb04483e21fac778d65c8741192177234390f
wongbingg <wb277@naver.com>
2023-02-23 21:12:01 +0900

    refactor : MovieDetailData 수정 #3
    - 타입메서드 추가

--- BoxOffice2/Domain/Entities/MovieDetailData.swift
+++ BoxOffice2/Domain/Entities/MovieDetailData.swift
@@ -7,7 +7,7 @@
 
 struct MovieDetailData {
     let uuid: String
-    let posterURL: String
+    var posterURL: String
     let currentRank: String
     let title: String
     let openDate: String
@@ -21,5 +21,42 @@ struct MovieDetailData {
     let directorName: String
     let actors: [String]
     let ageLimit: String
+    let movieCode: String
+}
+
+extension MovieDetailData {
+    static func stub(uuid: String = "" ,
+                     posterURL: String = "",
+                     currentRank: String = "",
+                     title: String = "",
+                     openDate: String = "",
+                     totalAudience: String = "",
+                     rankChange: String = "",
+                     isNewEntry: Bool = false,
+                     productionYear: String = "",
+                     openYear: String = "",
+                     showTime: String = "",
+                     genreName: String = "",
+                     directorName: String = "",
+                     actors: [String] = [],
+                     ageLimit: String = "",
+                     movieCode: String = "") -> Self {
+        .init(uuid: uuid,
+              posterURL: posterURL,
+              currentRank: currentRank,
+              title: title,
+              openDate: openDate,
+              totalAudience: totalAudience,
+              rankChange: rankChange,
+              isNewEntry: isNewEntry,
+              productionYear: productionYear,
+              openYear: openYear,
+              showTime: showTime,
+              genreName: genreName,
+              directorName: directorName,
+              actors: actors,
+              ageLimit: ageLimit,
+              movieCode: movieCode)
+    }
 }
 
