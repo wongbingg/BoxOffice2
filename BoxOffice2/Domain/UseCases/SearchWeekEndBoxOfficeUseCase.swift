63bb6323a4e809241c5b5ef8614ffb4fbab1d5f7
wongbingg <wb277@naver.com>
2023-02-23 21:19:56 +0900

    feat : SearchWeekEndBoxOfficeUseCase 생성 #3
    - 주말 박스오피스 정보를 요청하는 UseCase 생성

---
+++ BoxOffice2/Domain/UseCases/SearchWeekEndBoxOfficeUseCase.swift
@@ -0,0 +1,28 @@
+//
+//  SearchWeekEndBoxOfficeUseCase.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/23.
+//
+
+import RxSwift
+
+protocol SearchWeekEndBoxOfficeUseCase {
+    func execute(date: String) -> Observable<[MovieCellData]>
+}
+
+struct DefaultSearchWeekEndBoxOfficeUseCase: SearchWeekEndBoxOfficeUseCase {
+    let movieRepository: MovieRepository
+    
+    func execute(date: String) -> Observable<[MovieCellData]> {
+        
+        return Observable.create { emitter in
+            movieRepository.fetchWeekEndMovies(date: date)
+                .subscribe { movies in
+                    emitter.onNext(movies)
+                } onError: { error in
+                    emitter.onError(error)
+                }
+        }
+    }
+}
