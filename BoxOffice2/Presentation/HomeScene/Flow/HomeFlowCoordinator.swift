d29bb03642967b7098924a009ef8c3c9b11c72cd
wongbingg <wb277@naver.com>
2023-02-24 17:38:10 +0900

    feat : HomeFlowCoordinator 생성 #5
    - HomeScene의 화면전환을 담당

---
+++ BoxOffice2/Presentation/HomeScene/Flow/HomeFlowCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  HomeFlowCoordinator.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/24.
+//
+
+import UIKit
+
+protocol HomeFlowCoordinatorDependencies: AnyObject {
+    func makeHomeViewController(actions: HomeViewModelActions) -> HomeViewController
+}
+
+final class HomeFlowCoordinator {
+    private let navigationController: UINavigationController
+    private let dependencies: HomeFlowCoordinatorDependencies
+    
+    init(
+        navigationController: UINavigationController,
+        dependencies: HomeFlowCoordinatorDependencies
+    ) {
+        self.navigationController = navigationController
+        self.dependencies = dependencies
+    }
+    
+    func start() {
+        let actions = HomeViewModelActions(showMovieDetail: showMovieDetail)
+        let homeVC = dependencies.makeHomeViewController(actions: actions)
+        navigationController.pushViewController(homeVC, animated: true)
+    }
+    
+    // MARK: - View Transition
+    private func showMovieDetail(with movieCellData: MovieCellData) {
+        // 다음씬 생성 후 이동
+    }
+}
