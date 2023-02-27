bfa584b1f3a5c0dbaffe7c1017f21d6928696644
wongbingg <wb277@naver.com>
2023-02-27 12:02:30 +0900

    refactor : String+Extension을 Extension 그룹으로 이동

---
+++ BoxOffice2/Presentation/Util/Extensions/String+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  String+Extension.swift
+//  BoxOffice2
+//
+//  Created by 이원빈 on 2023/02/26.
+//
+
+import Foundation
+
+extension String {
+    var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }
+    
+    func toDecimal() -> String {
+        guard let number = Int(self) else { return "" }
+        return numberFormatter.string(from: NSNumber(integerLiteral: number)) ?? ""
+    }
+}
+
+extension String: Entity {}
