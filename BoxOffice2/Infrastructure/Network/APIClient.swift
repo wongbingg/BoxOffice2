f9e9e431ca886a999633e3c5d3fc6fc3ea84c3d7
wongbingg <wb277@naver.com>
2023-02-27 20:46:43 +0900

    refactor : 테스트를 위한 URLSession 추상화 #5

--- BoxOffice2/Infrastructure/Network/APIClient.swift
+++ BoxOffice2/Infrastructure/Network/APIClient.swift
@@ -10,20 +10,20 @@ import RxSwift
 
 struct APIClient {
     static let shared = APIClient(sesseion: URLSession.shared)
-    private let session: URLSession
+    private let session: URLSessionProtocol
     
     func requestData(with urlRequest: URLRequest) -> Observable<Data> {
         
         return Observable.create { emitter in
             let task = session.dataTask(with: urlRequest) { data, response, error in
-
+                
                 guard error == nil else {
                     emitter.onError(APIError.unknown)
                     return
                 }
                 let successRange = 200..<300
                 if let statusCode = (response as? HTTPURLResponse)?.statusCode,
-                      successRange.contains(statusCode) == false {
+                   successRange.contains(statusCode) == false {
                     emitter.onError(APIError.response(statusCode))
                     return
                 }
@@ -37,12 +37,23 @@ struct APIClient {
             task.resume()
             
             return Disposables.create {
-                task.cancel()
+//                task.cancel()
+                //이게 있음면 mockURLSession 테스트에서 계속 cancel오류가 뜸 ..
             }
         }
     }
-
-    init(sesseion: URLSession) {
+    
+    init(sesseion: URLSessionProtocol) {
         self.session = sesseion
     }
 }
+
+// MARK: - URLSession Protocol
+protocol URLSessionProtocol {
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol {}
