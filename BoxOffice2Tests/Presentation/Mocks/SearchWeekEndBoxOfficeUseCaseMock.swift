d63ad6fdc51c420c0336096a11990a36d218f757
wongbingg <wb277@naver.com>
2023-02-27 22:16:14 +0900

    feat : SearchWeekEndBoxOfficeUseCaseMock 생성 #7

---
+++ BoxOffice2Tests/Presentation/Mocks/SearchWeekEndBoxOfficeUseCaseMock.swift
@@ -0,0 +1,19 @@
+//
+//  SearchWeekEndBoxOfficeUseCaseMock.swift
+//  BoxOffice2Tests
+//
+//  Created by 이원빈 on 2023/02/27.
+//
+
+@testable import BoxOffice2
+import RxSwift
+
+final class SearchWeekEndBoxOfficeUseCaseMock: SearchWeekEndBoxOfficeUseCase {
+    var callCount = 0
+    private let movieReopsitory = MovieRepositoryMock()
+    
+    func execute(date: String) -> Observable<[MovieCellData]> {
+        callCount += 1
+        return movieReopsitory.fetchWeekEndMovies(date: date)
+    }
+}
